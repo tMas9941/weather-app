c0ca1cb96d65ca8107f65c8aa4811c29407c030d
m.tamas <tamasm9941@gmail.com>
2025-05-26 18:40:31 +0200

    add auto refresh city data

--- src/hooks/useCityWeather.js
+++ src/hooks/useCityWeather.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import WEATHER_API_KEY from "../constants/keys.js/";
+import { citiesList } from "../global/citiesData";
+
+const INTERVAL = 300000;
 
 export default function useCityWeather(city) {
 	const [data, setData] = useState();
+	let ready = false;
 
 	useEffect(() => {
-		let ready = false;
 		if (!ready) {
-			fetch(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`)
-				.then((response) => (response.ok ? response.json() : null))
-				.then((json) => setData(json));
+			fetchData();
 		}
-
 		return () => {
 			ready = true;
 		};
 	}, [city]);
+
+	function fetchData() {
+		fetch(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`)
+			.then((response) => (response.ok ? response.json() : null))
+			.then((json) => setData(json))
+			.then(() => setTimeout(() => citiesList.value.includes(city) && fetchData(), INTERVAL));
+	}
 
 	return data;
 }
